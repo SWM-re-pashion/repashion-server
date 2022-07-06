c09dd4dbb5e1d619a89a1d5cd4c8eb798bac83b9
DongWooE <ehddn977@naver.com>
2022-07-06 11:26:05 +0900

    :bug: SOMA-201/bug cors 오류 해결

--- server/server/src/main/java/rePashion/server/domain/user/controller/KakaoLoginController.java
+++ server/server/src/main/java/rePashion/server/domain/user/controller/KakaoLoginController.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rePashion.server.domain.user.dto.KakaoLoginRequestDto;
 import rePashion.server.domain.user.dto.KakaoLoginResponseDto;
 import rePashion.server.domain.user.dto.KakaoUserInfoDto;
@@ -16,6 +13,7 @@ import rePashion.server.domain.user.service.KakaoSignInService;
 import javax.validation.Valid;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/kakao")
 @RequiredArgsConstructor
 public class KakaoLoginController {
