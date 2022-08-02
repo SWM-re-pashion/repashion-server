2fda50c064b500993859a0f22a3eb3efb0f389da
DongWooE <ehddn977@naver.com>
2022-08-03 02:10:34 +0900

    :white_check_mark: SOMA-275/feat/#i18 Service 테스트 코드 작성 시작

---
+++ server/server/src/test/java/rePashion/server/domain/product/service/ProductDetailServiceTest.java
@@ -0,0 +1,17 @@
+package rePashion.server.domain.product.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ProductDetailServiceTest {
+
+    @InjectMocks
+    private ProductDetailService productDetailService;
+
+    @Test
+    public void foo(){
+    }
+}
