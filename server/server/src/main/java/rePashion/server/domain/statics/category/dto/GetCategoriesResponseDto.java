93b77b6d4362d66fd7a43aec63e918aa92d623ac
DongWooE <ehddn977@naver.com>
2022-09-08 12:19:40 +0900

    :sparkles: SOMA-393 category api 수정

--- server/server/src/main/java/rePashion/server/domain/statics/category/dto/GetCategoriesResponseDto.java
+++ server/server/src/main/java/rePashion/server/domain/statics/category/dto/GetCategoriesResponseDto.java
@@ -14,11 +14,13 @@ public class GetCategoriesResponseDto {
 
     @Getter
     public static class TopCategory{
+        private String id;
         private String name;
         private String code;
         private ArrayList<MiddleCategory> children;
 
-        public TopCategory(String name, String code, ArrayList<MiddleCategory> children) {
+        public TopCategory(String id, String name, String code, ArrayList<MiddleCategory> children) {
+            this.id = id;
             this.name = name;
             this.code = code;
             this.children = children;
@@ -27,11 +29,13 @@ public class GetCategoriesResponseDto {
 
     @Getter
     public static class MiddleCategory{
+        private String id;
         private String name;
         private String code;
         private ArrayList<BottomCategory> children;
 
-        public MiddleCategory(String name, String code, ArrayList<BottomCategory> children) {
+        public MiddleCategory(String id, String name, String code, ArrayList<BottomCategory> children) {
+            this.id = id;
             this.name = name;
             this.code = code;
             this.children = children;
@@ -40,19 +44,20 @@ public class GetCategoriesResponseDto {
 
     @Getter
     public static class BottomCategory{
+        private String id;
         private String name;
         private String code;
 
-        public BottomCategory(String name, String code) {
+        public BottomCategory(String id, String name, String code) {
+            this.id = id;
             this.name = name;
             this.code = code;
         }
     }
 
-    private ArrayList<TopCategory> children = new ArrayList<>();
     private final String name = "성별";
     private final String code = "gender";
-
+    private ArrayList<TopCategory> children = new ArrayList<>();
     public GetCategoriesResponseDto(ArrayList<TopCategory> children) {
         this.children = children;
     }
@@ -64,13 +69,13 @@ public class GetCategoriesResponseDto {
             gender.getChildrens().forEach((parent)->{
                 ArrayList<BottomCategory> bottomCategories = new ArrayList<>();
                 parent.getChildrens().forEach((sub)->{
-                    BottomCategory bottomCategory = new BottomCategory(sub.getName(), sub.getCode());
+                    BottomCategory bottomCategory = new BottomCategory(String.valueOf(sub.getId()), sub.getName(), sub.getCode());
                     bottomCategories.add(bottomCategory);
                 });
-                MiddleCategory middleCategory = new MiddleCategory(parent.getName(), parent.getCode(), bottomCategories);
+                MiddleCategory middleCategory = new MiddleCategory(String.valueOf(parent.getId()), parent.getName(), parent.getCode(), bottomCategories);
                 middleCategories.add(middleCategory);
             });
-            TopCategory topCategory = new TopCategory(gender.getName(), gender.getCode(), middleCategories);
+            TopCategory topCategory = new TopCategory(String.valueOf(gender.getId()), gender.getName(), gender.getCode(), middleCategories);
             topCategories.add(topCategory);
         });
         return new GetCategoriesResponseDto(topCategories);
--- server/server/src/main/java/rePashion/server/domain/statics/category/service/CategoryService.java
+++ server/server/src/main/java/rePashion/server/domain/statics/category/service/CategoryService.java
@@ -4,27 +4,37 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import rePashion.server.domain.statics.category.dto.GetCategoriesResponseDto;
 import rePashion.server.domain.statics.category.model.GenderCategory;
+import rePashion.server.domain.statics.category.model.ParentCategory;
+import rePashion.server.domain.statics.category.model.SubCategory;
 import rePashion.server.domain.statics.category.repository.GenderCategoryRepository;
+import rePashion.server.domain.statics.category.repository.ParentCategoryRepository;
+import rePashion.server.domain.statics.category.repository.SubCategoryRepository;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
 
     private final GenderCategoryRepository genderCategoryRepository;
-    private final AddInitialCategoryService addBasicCategoryService;
+    private final ParentCategoryRepository parentCategoryRepository;
+    private final SubCategoryRepository subCategoryRepository;
+    private final InitCategoryService initCategoryService;
 
     public GetCategoriesResponseDto getV1(){
-        addBasicCategoryService.addInitialData();
+        initCategoryService.initData();
         List<GenderCategory> genderCategories = genderCategoryRepository.findAllCategoriesV1();
         GetCategoriesResponseDto getCategoriesResponseDto = GetCategoriesResponseDto.fromEntity(genderCategories);
         return getCategoriesResponseDto;
     }
 
     public GetCategoriesResponseDto getV2(){
-        addBasicCategoryService.addInitialData();
-        List<GenderCategory> genderCategories = genderCategoryRepository.findAllCategoriesV2();
+        initCategoryService.initData();
+        List<GenderCategory> genderCategories = genderCategoryRepository.findAllGenderCategories();
+        //genderCategories.forEach(g->parentCategoryRepository.findAllParentCategoriesById(g.getId()).forEach(s->subCategoryRepository.findAllSubCategoriesById(s.getId())));
         GetCategoriesResponseDto getCategoriesResponseDto = GetCategoriesResponseDto.fromEntity(genderCategories);
         return getCategoriesResponseDto;
     }
