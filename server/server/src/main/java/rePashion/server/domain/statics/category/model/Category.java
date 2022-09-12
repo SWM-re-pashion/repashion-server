c7b226fe6781aaeb91724e104f8dfecd0d1897ad
DongWooE <ehddn977@naver.com>
2022-09-13 00:25:14 +0900

    :card_file_box: SOMA-413 category 모델 생성

---
+++ server/server/src/main/java/rePashion/server/domain/statics/category/model/Category.java
@@ -0,0 +1,57 @@
+package rePashion.server.domain.statics.category.model;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static javax.persistence.CascadeType.PERSIST;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+    @Id
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_category_id")
+    private Category parentCategory;
+
+    @OrderBy("categoryId asc")
+    @OneToMany(mappedBy = "parentCategory", cascade = PERSIST)
+    private Set<Category> children = new HashSet<>();
+
+    private String name;
+
+    private String code;
+
+    private int depth;
+
+    @Builder
+    public Category(Long categoryId, String name, String code) {
+        this.categoryId = categoryId;
+        this.name = name;
+        this.code = code;
+    }
+
+    public void updateParent(Category parent) {
+        if(parent == null){
+            this.parentCategory = null;
+            this.depth = 1;
+        }
+        else{
+            this.parentCategory = parent;
+            this.depth = parent.getDepth()+1;
+            parent.getChildren().add(this);
+        }
+    }
+}
