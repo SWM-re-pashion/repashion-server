55a47f18ee5e9c5c40b6cdc2894c3b22a77980f4
DongWooE <ehddn977@naver.com>
2022-09-13 00:26:32 +0900

    :sparkles: SOMA-413 dto 재정의 및 service 코드 수정

--- server/server/src/main/java/rePashion/server/domain/statics/category/service/CategoryService.java
+++ server/server/src/main/java/rePashion/server/domain/statics/category/service/CategoryService.java
@@ -2,18 +2,19 @@ package rePashion.server.domain.statics.category.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import rePashion.server.domain.statics.category.dto.CategoryResponseDto;
 import rePashion.server.domain.statics.category.dto.GetCategoriesResponseDto;
+import rePashion.server.domain.statics.category.model.Category;
 import rePashion.server.domain.statics.category.model.GenderCategory;
 import rePashion.server.domain.statics.category.model.ParentCategory;
 import rePashion.server.domain.statics.category.model.SubCategory;
+import rePashion.server.domain.statics.category.repository.CategoryRepository;
 import rePashion.server.domain.statics.category.repository.GenderCategoryRepository;
 import rePashion.server.domain.statics.category.repository.ParentCategoryRepository;
 import rePashion.server.domain.statics.category.repository.SubCategoryRepository;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +23,7 @@ public class CategoryService {
     private final GenderCategoryRepository genderCategoryRepository;
     private final ParentCategoryRepository parentCategoryRepository;
     private final SubCategoryRepository subCategoryRepository;
+    private final CategoryRepository categoryRepository;
     private final InitCategoryService initCategoryService;
 
     public GetCategoriesResponseDto getV1(){
@@ -37,5 +39,12 @@ public class CategoryService {
         //genderCategories.forEach(g->parentCategoryRepository.findAllParentCategoriesById(g.getId()).forEach(s->subCategoryRepository.findAllSubCategoriesById(s.getId())));
         GetCategoriesResponseDto getCategoriesResponseDto = GetCategoriesResponseDto.fromEntity(genderCategories);
         return getCategoriesResponseDto;
+    }
+
+    public CategoryResponseDto getV3(){
+        List<CategoryResponseDto> collect = categoryRepository.findTopCategories()
+                .stream().map(CategoryResponseDto::fromEntity)
+                .collect(Collectors.toList());
+        return new CategoryResponseDto("성별", "gender", collect);
     }
 }
