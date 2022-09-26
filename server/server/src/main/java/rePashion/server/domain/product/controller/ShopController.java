e1891530faa76c45594e96b0aa43b858edb31b65
DongWooE <ehddn977@naver.com>
2022-09-27 00:24:55 +0900

    :sparkles: SOMA-434 shop filter controller 추가

--- server/server/src/main/java/rePashion/server/domain/product/controller/ShopController.java
+++ server/server/src/main/java/rePashion/server/domain/product/controller/ShopController.java
@@ -8,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rePashion.server.domain.product.dto.ProductFilterCond;
 import rePashion.server.domain.product.dto.ProductPreviewDto;
 import rePashion.server.domain.product.dto.ProductSearchCond;
 import rePashion.server.domain.product.dto.response.ShopResponseDto;
+import rePashion.server.domain.product.repository.ProductFilterRepository;
 import rePashion.server.domain.product.repository.ProductSearchRepository;
 import rePashion.server.global.common.response.GlobalResponse;
 import rePashion.server.global.common.response.StatusCode;
@@ -20,10 +22,18 @@ import rePashion.server.global.common.response.StatusCode;
 @RequiredArgsConstructor
 public class ShopController {
     private final ProductSearchRepository productSearchRepository;
+    private final ProductFilterRepository productFilterRepository;
 
     @GetMapping
     public ResponseEntity<GlobalResponse> getShop(ProductSearchCond cond, Pageable pageable){
         Page<ProductPreviewDto> productPreview = productSearchRepository.search(cond, pageable);
+        ShopResponseDto response = toShopResponseEntity(productPreview);
+        return new ResponseEntity<>(GlobalResponse.of(StatusCode.SUCCESS, response), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<GlobalResponse> getShopByFilter(ProductFilterCond cond, Pageable pageable){
+        Page<ProductPreviewDto> productPreview = productFilterRepository.get(cond, pageable);
         ShopResponseDto response = toShopResponseEntity(productPreview);
         return new ResponseEntity<>(GlobalResponse.of(StatusCode.SUCCESS, response), HttpStatus.OK);
     }
