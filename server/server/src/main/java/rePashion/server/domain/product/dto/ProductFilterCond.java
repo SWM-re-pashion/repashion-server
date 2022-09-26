576764e0efab28c9616ee32fa6077506a974a9d0
DongWooE <ehddn977@naver.com>
2022-09-26 16:06:28 +0900

    :sparkles: SOMA-434 filter condition 정의

---
+++ server/server/src/main/java/rePashion/server/domain/product/dto/ProductFilterCond.java
@@ -0,0 +1,20 @@
+package rePashion.server.domain.product.dto;
+
+import lombok.Data;
+import rePashion.server.domain.statics.model.filter.Order;
+
+import java.util.List;
+
+@Data
+public class ProductFilterCond {
+    private String category;
+    private List<String> style;
+    private List<String> color;
+    private List<String> fit;
+    private List<String> length;
+    private List<String> size;
+    private Order order;
+    private Boolean hideSold;
+    private Integer priceLoe;
+    private Integer priceGoe;
+}
