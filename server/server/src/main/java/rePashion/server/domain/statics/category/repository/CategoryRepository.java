90cc4b6d3b6ffbcef1be47d7a335f67375e25754
DongWooE <ehddn977@naver.com>
2022-09-28 12:50:17 +0900

    :adhesive_bandage: feat/#i63 상품 디테일 정보 가져오기 api 오류 수정
    Category repository 및 Service 코드 수정

--- server/server/src/main/java/rePashion/server/domain/statics/category/repository/CategoryRepository.java
+++ server/server/src/main/java/rePashion/server/domain/statics/category/repository/CategoryRepository.java
@@ -2,12 +2,17 @@ package rePashion.server.domain.statics.category.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import rePashion.server.domain.statics.category.model.Category;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     @Query("select distinct c from Category c left join fetch c.children cc left join fetch cc.children where c.categoryId <= 9L")
     List<Category> findTopCategories();
+
+    @Query("select c from Category c where c.categoryId = :id")
+    Optional<Category> findCategoryByCategoryId(@Param("id") Long id);
 }
