0a11752136b79bb32716aaca831ab98eee4b2b43
DongWooE <ehddn977@naver.com>
2022-10-05 01:00:15 +0900

    :ambulance: hotfix Cors 해결 및 health-check permitting

--- server/server/src/main/java/rePashion/server/global/common/config/SpringSecurityConfig.java
+++ server/server/src/main/java/rePashion/server/global/common/config/SpringSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import rePashion.server.global.jwt.filter.JwtAuthorizationFilter;
 import rePashion.server.global.jwt.impl.AccessTokenProvider;
 
@@ -20,13 +21,15 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable();
 
         http.authorizeRequests()
-//                .antMatchers("/health-check").permitAll()
+                .antMatchers("/health-check").permitAll()
                 .antMatchers("/api/auth/**").permitAll()       //api/users/**는 로그인, 중복 id 검사 등등 이므로 모든 권한을 가진 자들에게 공개
                 .anyRequest().authenticated()                              // 그 밖에 모든 요청은 jwt를 통해서
                 .and()
