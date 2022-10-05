45ef273be505d65ff5832fd3f0564cf30882d7b3
DongWooE <ehddn977@naver.com>
2022-10-06 04:35:10 +0900

    :sparkles: SOMA-397 nickname 바꾸기 api 추가

--- server/server/src/main/java/rePashion/server/domain/user/repository/UserRepository.java
+++ server/server/src/main/java/rePashion/server/domain/user/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package rePashion.server.domain.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import rePashion.server.domain.user.model.User;
+
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,4 +17,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("select u from User u where u.email = :email")
     Optional<User> findUserByEmail(@Param("email") String email);
+
+    @Transactional
+    @Modifying
+    @Query("update User u set u.nickName = :nickName where u.id = :id")
+    void updateUserNickName(@Param("nickName") String nickName, @Param("id") Long id);
 }
