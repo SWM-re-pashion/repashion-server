43b4f26dc3c322be033492696b15c20f9474d44f
DongWooE <ehddn977@naver.com>
2022-10-06 01:47:19 +0900

    :white_check_mark: SOMA-419 내정보불러오기 api passing test 추가

---
+++ server/server/src/test/java/rePashion/server/domain/user/controller/MyUserControllerTest.java
@@ -0,0 +1,118 @@
+package rePashion.server.domain.user.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import rePashion.server.domain.auth.model.RefreshToken;
+import rePashion.server.domain.product.model.Product;
+import rePashion.server.domain.product.model.embedded.BasicInfo;
+import rePashion.server.domain.product.repository.ProductRepository;
+import rePashion.server.domain.user.model.*;
+import rePashion.server.domain.user.repository.UserProductRepository;
+import rePashion.server.domain.user.repository.UserRepository;
+import rePashion.server.global.jwt.impl.AccessTokenProvider;
+
+import javax.servlet.http.Cookie;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MyUserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Value("${auth.jwt.access.header}")
+    private String header;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserProductRepository userProductRepository;
+
+    @Autowired
+    private AccessTokenProvider accessTokenProvider;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    private ArrayList<Product> productBuyList = new ArrayList<>();
+
+    private ArrayList<Product> productSellList = new ArrayList<>();
+
+    @BeforeEach
+    public void initProductDb(){
+        for(int i =0; i < 5; i++){
+            productBuyList.add(createProduct());
+        }
+
+        for(int i =0; i < 4; i++){
+            productSellList.add(createProduct());
+        }
+    }
+
+    private Product createProduct() {
+
+        BasicInfo basicInfo = BasicInfo.builder()
+                .title("title01")
+                .contact("test01@test.com")
+                .category("1001001")
+                .brand("brand01")
+                .thumbnailImage("https://image01")
+                .price(200000)
+                .isIncludeDelivery(true)
+                .size("XS")
+                .build();
+
+        Product product = Product.builder().basicInfo(basicInfo).build();
+        return productRepository.save(product);
+    }
+
+    @Test
+    public void 정상적인_내정보_받기() throws Exception {
+        //given
+
+        // User 만들기 및 accessToken 토큰 만들기
+        User user = new User("test@test.com", "", "hi");
+        UserAuthority userAuthority1 = new UserAuthority(Role.ROLE_USER);
+        userAuthority1.changeAuthority(user);
+        User savedUser = userRepository.save(user);
+        String parsedAccessToken = accessTokenProvider.parse(savedUser);
+
+        // User가 판매, 구매중인 상품들 만들기
+        productBuyList.forEach(product -> {
+            UserProduct userProduct = new UserProduct(PurchaseStatus.Buyer);
+            userProduct.changeUserAndProduct(savedUser, product);
+            userProductRepository.save(userProduct);
+        });
+
+        productSellList.forEach(product -> {
+            UserProduct userProduct = new UserProduct(PurchaseStatus.Seller);
+            userProduct.changeUserAndProduct(savedUser, product);
+            userProductRepository.save(userProduct);
+        });
+
+        int totalCount = userProductRepository.getNumberOfPurchasingByUser(savedUser.getId());
+        System.out.println("totalCount = " + totalCount);
+
+        //when
+        //then
+        mvc.perform(get("/api/user/my").header(header, parsedAccessToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.name", is(savedUser.getNickName())))
+                .andExpect(jsonPath("$.data.profileImage", is(savedUser.getProfile())))
+                .andExpect(jsonPath("$.data.totalCount", is(9)));
+    }
+}
