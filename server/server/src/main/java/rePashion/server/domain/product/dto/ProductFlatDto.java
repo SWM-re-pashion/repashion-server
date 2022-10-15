e712a40804e83f8eba79ea2ddef9c1302f82d4f3
DongWooE <ehddn977@naver.com>
2022-10-15 23:25:05 +0900

    :adhesive_bandage: SOMA-460 flatDto 정의 및 기존 ProductDto 수정

---
+++ server/server/src/main/java/rePashion/server/domain/product/dto/ProductFlatDto.java
@@ -0,0 +1,33 @@
+package rePashion.server.domain.product.dto;
+
+
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+public class ProductFlatDto {
+    private ArrayList<String> imgList;
+    private String contact;
+    private String title;
+    private String category;
+    private String brand;
+    private int price;
+    private Boolean isIncludeDelivery;
+    private String size;
+    private String purchaseTime;
+    private String purchasePlace;
+    private String condition;
+    private String pollution;
+    private int height;
+    private String bodyShape;
+    private String length;
+    private String fit;
+    private String tag;
+    private String color;
+    private String material;
+    private ProductRequestBody.Style style;
+    private ProductRequestBody.Measure measure;
+    private String opinion;
+    private String measureType;
+}
--- server/server/src/main/java/rePashion/server/domain/product/dto/ProductRequestBody.java
+++ server/server/src/main/java/rePashion/server/domain/product/dto/ProductRequestBody.java
@@ -1,53 +1,30 @@
 package rePashion.server.domain.product.dto;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import rePashion.server.domain.product.model.Product;
+import lombok.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ProductCreateDto {
-    private ArrayList<String> imgList;
-    private String contact;
-
+public class ProductRequestBody {
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
     public static class BasicInfo{
         private String title;
         private String category;
         private String brand;
-
-        public BasicInfo(String title, String category, String brand) {
-            this.title = title;
-            this.category = category;
-            this.brand = brand;
-        }
     }
-    private BasicInfo basicInfo;
-    private int price;
-    private Boolean isIncludeDelivery;
-    private String size;
-
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
     public static class AdditionalInfo{
         private String purchaseTime;
         private String purchasePlace;
-
-        public AdditionalInfo(String purchaseTime, String purchasePlace) {
-            this.purchaseTime = purchaseTime;
-            this.purchasePlace = purchasePlace;
-        }
     }
-    private AdditionalInfo additionalInfo;
 
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
     public static class SellerNote{
         private String condition;
         private String pollution;
@@ -55,39 +32,45 @@ public class ProductCreateDto {
         private String bodyShape;
         private String length;
         private String fit;
-
-        public SellerNote(String condition, String pollution, int height, String bodyShape, String length, String fit) {
-            this.condition = condition;
-            this.pollution = pollution;
-            this.height = height;
-            this.bodyShape = bodyShape;
-            this.length = length;
-            this.fit = fit;
-        }
     }
-    private SellerNote sellerNote;
 
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
     public static class Style{
         private String tag;
         private String color;
         private String material;
-
-        public Style(String tag, String color, String material) {
-            this.tag = tag;
-            this.color = color;
-            this.material = material;
-        }
     }
-    private Style style;
-    private HashMap<String, Integer> measure;
-    private String opinion;
 
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    public static class Measure{
+        private Integer length;
+        private Integer shoulderWidth;
+        private Integer chestSection;
+        private Integer sleeveLength;
+        private Integer waistSection;
+        private Integer bottomSection;
+        private Integer thighSection;
+        private Integer rise;
+    }
+
+    private ArrayList<String> imgList;
+    private String contact;
+    private BasicInfo basicInfo;
+    private int price;
+    private Boolean isIncludeDelivery;
+    private String size;
+    private AdditionalInfo additionalInfo;
+    private SellerNote sellerNote;
+    private Style style;
+    private Measure measure;
+    private String opinion;
     private String measureType;
 
-    @Builder
-    public ProductCreateDto(ArrayList<String> imgList, String contact, BasicInfo basicInfo, int price, Boolean isIncludeDelivery, String size, AdditionalInfo additionalInfo, SellerNote sellerNote, Style style, HashMap<String, Integer> measure, String opinion, String measureType) {
+    public ProductRequestBody(ArrayList<String> imgList, String contact, BasicInfo basicInfo, int price, Boolean isIncludeDelivery, String size, AdditionalInfo additionalInfo, SellerNote sellerNote, Style style, Measure measure, String opinion, String measureType) {
         this.imgList = imgList;
         this.contact = contact;
         this.basicInfo = basicInfo;
@@ -102,24 +85,51 @@ public class ProductCreateDto {
         this.measureType = measureType;
     }
 
-    static public ProductCreateDto toDto(Product product){
-        BasicInfo basicInfo = new BasicInfo(product.getBasicInfo().getTitle(), product.getBasicInfo().getCategory(), product.getBasicInfo().getBrand());
-        AdditionalInfo additionalInfo = new AdditionalInfo(product.getAdvanceInfo().getSellerNote().getPurchaseTime(), product.getAdvanceInfo().getSellerNote().getPurchasePlace());
-        SellerNote sellerNote = new SellerNote(product.getAdvanceInfo().getSellerNote().getConditions(), product.getAdvanceInfo().getSellerNote().getPollution(), product.getAdvanceInfo().getSellerNote().getHeight(), product.getAdvanceInfo().getSellerNote().getBodyShape(), product.getAdvanceInfo().getSellerNote().getLength(), product.getAdvanceInfo().getSellerNote().getFit());
-        Style style = new Style(product.getAdvanceInfo().getSellerNote().getTag(), product.getAdvanceInfo().getSellerNote().getColor(), product.getAdvanceInfo().getSellerNote().getMaterial());
-        HashMap<String, Integer> measure = product.getAdvanceInfo().getMeasure().getMap();
-        return ProductCreateDto.builder()
-                .imgList(product.toStringArray())
-                .contact(product.getBasicInfo().getContact())
-                .basicInfo(basicInfo)
-                .price(product.getBasicInfo().getPrice())
-                .isIncludeDelivery(product.getBasicInfo().isIncludeDelivery())
-                .size(product.getBasicInfo().getSize())
-                .additionalInfo(additionalInfo)
-                .sellerNote(sellerNote)
-                .style(style)
-                .measure(measure)
-                .opinion(product.getAdvanceInfo().getSellerNote().getOpinion())
-                .build();
+    public ArrayList<String> getImgList() {
+        return imgList;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public BasicInfo getBasicInfo() {
+        return basicInfo;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Boolean getIncludeDelivery() {
+        return isIncludeDelivery;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public AdditionalInfo getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public SellerNote getSellerNote() {
+        return sellerNote;
+    }
+
+    public Style getStyle() {
+        return style;
+    }
+
+    public Measure getMeasure() {
+        return measure;
+    }
+
+    public String getOpinion() {
+        return opinion;
+    }
+
+    public String getMeasureType() {
+        return measureType;
     }
 }
