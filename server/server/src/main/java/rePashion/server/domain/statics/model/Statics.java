31e00bb04ee4443903189c0f665ead08b5a1bece
DongWooE <ehddn977@naver.com>
2022-10-23 20:42:51 +0900

    :rewind: SOMA-462 실수로 삭제한 static 변수들 되돌림

---
+++ server/server/src/main/java/rePashion/server/domain/statics/model/Statics.java
@@ -0,0 +1,41 @@
+package rePashion.server.domain.statics.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Statics {
+
+    public static enum Type{
+        Style, Color, Fit, Length, Gender, Size, BodyShape, PollutionCondition, OrderDate
+    }
+    public static enum Classification{
+        top, bottom, common
+    }
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Type type;
+
+    private Classification classification;
+
+    private String name;
+
+    private String code;
+
+    public Statics(Type type, Classification classification, String name, String code) {
+        this.type = type;
+        this.classification = classification;
+        this.name = name;
+        this.code = code;
+    }
+}
