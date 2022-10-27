d5eabaa56af1d34fee79bf4fc1b2349ab1a3ab2b
DongWooE <ehddn977@naver.com>
2022-10-27 23:59:30 +0900

    :adhesive_bandage: SOMA-470 update dto 수정

--- server/server/src/main/java/rePashion/server/domain/user/resources/Request.java
+++ server/server/src/main/java/rePashion/server/domain/user/resources/Request.java
@@ -6,7 +6,9 @@ import lombok.Data;
 public class Request {
 
     @Data
-    public static class UpdateNickNameRequest{
-        private String nickName;
+    public static class Update{
+        private String name;
+        private String profileImage;
+        private Integer totalCount;
     }
 }
