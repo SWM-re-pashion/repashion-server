3177c5cb55ba0e3e3aa279628048b002460ba738
DongWooE <ehddn977@naver.com>
2022-11-10 04:17:30 +0900

    :sparkles: SOMA-491 product update repository 추가

--- server/server/src/main/java/rePashion/server/domain/product/repository/ProductRepository.java
+++ server/server/src/main/java/rePashion/server/domain/product/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.parameters.P;
 import rePashion.server.domain.product.model.Product;
 
 import javax.transaction.Transactional;
@@ -18,4 +19,9 @@ public interface ProductRepository extends JpaRepository<Product, Long>, Product
     @Transactional
     @Query("update Product p set p.basicInfo.status = true where p.id = :id")
     void updateStatus(@Param("id") Long id);
+
+    @Modifying
+    @Transactional
+    @Query("update Product p set p.basicInfo.visited = :newVisited, p.basicInfo.views =:view where p.id = :id")
+    void updateVisitedAndView(@Param("newVisited") String newVisited, @Param("view") int view, @Param("id") Long id);
 }
