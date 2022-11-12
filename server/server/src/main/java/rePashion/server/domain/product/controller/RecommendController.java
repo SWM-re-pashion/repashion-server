63a244f839560f45caa9b9a9ca75305edec9c6dd
DongWooE <ehddn977@naver.com>
2022-11-12 21:39:38 +0900

    :adhesive_bandage: SOMA-500 ProductRecommendController 수정
    toDto 메소드 추가

--- server/server/src/main/java/rePashion/server/domain/product/controller/RecommendController.java
+++ server/server/src/main/java/rePashion/server/domain/product/controller/RecommendController.java
@@ -5,11 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rePashion.server.domain.product.dto.ProductRecommendBody;
+import rePashion.server.domain.product.dto.ProductRecommendDto;
 import rePashion.server.domain.product.exception.ProductNotExistedException;
 import rePashion.server.domain.product.model.Product;
 import rePashion.server.domain.product.model.ProductRecommend;
@@ -21,7 +19,8 @@ import rePashion.server.domain.product.resources.response.Dto;
 import rePashion.server.global.common.response.GlobalResponse;
 import rePashion.server.global.common.response.StatusCode;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/recommend")
@@ -35,13 +34,18 @@ public class RecommendController {
     @GetMapping
     public ResponseEntity<GlobalResponse> getShop(Condition.Recommend cond, Pageable pageable){
         Page<ProductRecommend> productRecommends = productRecommendCustomRepository.get(cond, pageable);
-        Dto.Shop.Pagination pagination = new Dto.Shop.Pagination(productRecommends.isLast(), productRecommends.getTotalElements(), productRecommends.getTotalPages());
-        Dto.Shop response = new Dto.Shop(productRecommends.getContent(), pagination);
+        Dto.Shop response = toDto(productRecommends);
         return new ResponseEntity<>(GlobalResponse.of(StatusCode.SUCCESS, response), HttpStatus.OK);
     }
 
+    private Dto.Shop toDto(Page<ProductRecommend> productRecommends) {
+        Dto.Shop.Pagination pagination = new Dto.Shop.Pagination(productRecommends.isLast(), productRecommends.getTotalElements(), productRecommends.getTotalPages());
+        List<ProductRecommendDto> collect = productRecommends.getContent().stream().map(ProductRecommendDto::new).collect(Collectors.toList());
+        return new Dto.Shop(collect, pagination);
+    }
+
     @PostMapping
-    public String create(ProductRecommendBody body){
+    public String create(@RequestBody ProductRecommendBody body){
         Product product = productRepository.findById(body.getProductId()).orElseThrow(ProductNotExistedException::new);
         Product association = productRepository.findById(body.getAssociationId()).orElseThrow(ProductNotExistedException::new);
         ProductRecommend productRecommend = new ProductRecommend(product, association);
